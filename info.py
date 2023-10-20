353e3a85d977595173383e989e50b6c72a449807
Rkm563 <147932740+Rkm563@users.noreply.github.com>
2023-10-20 09:19:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,8 +31,7 @@ auth_grp = environ.get('AUTH_GROUP')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Rkm632:Rkm633000@cluster0.dfjupvc.mongodb.net/?retryWrites=true&w=majority
-")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Rkm632:Rkm633000@cluster0.dfjupvc.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
