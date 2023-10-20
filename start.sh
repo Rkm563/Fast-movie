68f889dad290e88721b5a407ed1af2591084ca18
Rkm563 <147932740+Rkm563@users.noreply.github.com>
2023-10-20 07:10:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/AutoFilterAdvance /AutoFilterAdvance
+  git clone https://github.com/Rkm563/Fast-movie /Fast-movie
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoFilterAdvance
+  git clone $UPSTREAM_REPO /Fast-movie
 fi
 cd /AutoFilterAdvance
 pip3 install -U -r requirements.txt
